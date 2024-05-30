d684aa1f12f5cd528fa77fe1c652fb02140d25e8
KonovK <konov.ki@yandex.ru>
2024-05-30 08:30:13 +0300

    Домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import math.Factorial;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Factorial.factorialNumber(5));
     }
 }
--- src/math/Factorial.java
+++ src/math/Factorial.java
@@ -1,4 +1,12 @@
 package math;
 
 public class Factorial {
+
+    public static int factorialNumber(int number) {
+        int a = number;
+        for (int i = 1; i < a; i++){
+            number = number * i;
+        }
+        return number;
+    }
 }
