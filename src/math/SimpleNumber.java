a2ba58e02fe6dc472767863eda19ebde31507f13
KonovK <konov.ki@yandex.ru>
2024-05-31 12:03:47 +0300

    Домашняя работа

---
+++ src/math/SimpleNumber.java
@@ -0,0 +1,13 @@
+package math;
+
+public class SimpleNumber {
+
+    public static boolean simpleNumber(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
